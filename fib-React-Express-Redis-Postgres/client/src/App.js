f4be7c3c13e1e15a53ddb34122f31b8eac46cd65
spfscmz <psong4@dons.usfca.edu>
2018-11-08 23:30:31 -0800

    Get things going

--- fib-React-Express-Redis-Postgres/client/src/App.js
+++ fib-React-Express-Redis-Postgres/client/src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
-
 class App extends Component {
   render() {
     return (
@@ -13,8 +12,9 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
             <Link to="/">Home</Link>
-            <Link to="otherpage">Other Page</Link>
+            <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
